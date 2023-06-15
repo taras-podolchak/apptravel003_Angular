4f511ccc6e2329b833cda9bac5fc9a37991431e6
Taras <taraspodolchak@gmail.com>
2023-06-15 16:57:49 +0200

    configured continuous deployment on Heroku. issue #20

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+import pkg from '../../package.json';
+
 export const environment = {
-  production: true
+  production: true,
+  NAME: pkg.name,
+  VERSION: pkg.version,
+  REST_APPTRAVEL003_SPRING_BOOT: 'https://apptravel003-17ad2df26ee7.herokuapp.com',
 };
